28f2f0a977ecac75eceb8f155b6906cbb8845bc9
Kate Hannah <67842122+KVHannah@users.noreply.github.com>
2024-01-17 23:47:52 +1030

    Initial version January 2024

---
+++ README.md
@@ -0,0 +1,9 @@
+# docassemble.LDOJerseyFees
+
+An application for preparing instructions to give to the LDO for drafting amendments to fees Orders. This app is in use by departments.
+
+## Author
+
+Kate Hannah, k.hannah@gov.je
+Zoe Rillstone, z.rillstone@gov.je
+
---
+++ setup.py
@@ -0,0 +1,61 @@
+import os
+import sys
+from setuptools import setup, find_packages
+from fnmatch import fnmatchcase
+from distutils.util import convert_path
+
+standard_exclude = ('*.pyc', '*~', '.*', '*.bak', '*.swp*')
+standard_exclude_directories = ('.*', 'CVS', '_darcs', './build', './dist', 'EGG-INFO', '*.egg-info')
+
+def find_package_data(where='.', package='', exclude=standard_exclude, exclude_directories=standard_exclude_directories):
+    out = {}
+    stack = [(convert_path(where), '', package)]
+    while stack:
+        where, prefix, package = stack.pop(0)
+        for name in os.listdir(where):
+            fn = os.path.join(where, name)
+            if os.path.isdir(fn):
+                bad_name = False
+                for pattern in exclude_directories:
+                    if (fnmatchcase(name, pattern)
+                        or fn.lower() == pattern.lower()):
+                        bad_name = True
+                        break
+                if bad_name:
+                    continue
+                if os.path.isfile(os.path.join(fn, '__init__.py')):
+                    if not package:
+                        new_package = name
+                    else:
+                        new_package = package + '.' + name
+                        stack.append((fn, '', new_package))
+                else:
+                    stack.append((fn, prefix + name + '/', package))
+            else:
+                bad_name = False
+                for pattern in exclude:
+                    if (fnmatchcase(name, pattern)
+                        or fn.lower() == pattern.lower()):
+                        bad_name = True
+                        break
+                if bad_name:
+                    continue
+                out.setdefault(package, []).append(prefix+name)
+    return out
+
+setup(name='docassemble.LDOJerseyFees',
+      version='0.0.1',
+      description=('An application for creating instructions to amend fees Orders'),
+      long_description='# docassemble.LDOJerseyFees\r\n\r\nAn application for preparing instructions to give to the LDO for drafting amendments to fees Orders. This app is in use by departments.\r\n\r\n## Author\r\n\r\nKate Hannah, k.hannah@gov.je\r\nZoe Rillstone, z.rillstone@gov.je\r\n\r\n',
+      long_description_content_type='text/markdown',
+      author='Kate Hannah',
+      author_email='K.Hannah@gov.je',
+      license='The MIT License (MIT)',
+      url='https://docassemble.org',
+      packages=find_packages(),
+      namespace_packages=['docassemble'],
+      install_requires=[],
+      zip_safe=False,
+      package_data=find_package_data(where='docassemble/LDOJerseyFees/', package='docassemble.LDOJerseyFees'),
+     )
+
